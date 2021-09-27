ef057d3446d893566a4349452abbe3ec2f973374
Isradin <isradin.nurbek@gmail.com>
2021-09-27 20:55:11 +0300

    Create peoject Employee app

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Employee accounting at CRUD</h1>
+            <h2>Total number of employees</h2>
+            <h2>The cash prize is received by: </h2>
+        </div>
+    );
+};
+
+export default AppInfo;
