c26e661ba8d343b642853a06bdbf827077d99a1d
Isradin <isradin.nurbek@gmail.com>
2021-09-28 21:48:34 +0300

    Create component AppFilter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,6 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
 import './app.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
 
             <div className="search-panel">
                 <SearchPanel />
+                <AppFilter />
             </div>
         </div>
     );
---
+++ src/components/employees-list-item/employees-list-item.js
@@ -0,0 +1,7 @@
+const EmployeesListItem = () => {
+    <li className="list-group-item d-flex justify-content-between">
+        <span className="list-group-item-label">Isradin Nurbek</span>
+    </li>
+};
+
+export default EmployeesListItem;
