7cf8d71b9c86296a9afbc1c590aa18fc45ec21c5
Isradin <isradin.nurbek@gmail.com>
2021-09-30 21:23:58 +0300

    Create component employees-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,8 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+
 import './app.css';
 
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
                 <SearchPanel />
                 <AppFilter />
             </div>
+
+            <EmployeesList />
         </div>
     );
 };
