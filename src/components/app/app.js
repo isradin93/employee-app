ffa67c125a3dfdb19319b344c4c0fb44489bcaf3
Isradin <isradin.nurbek@gmail.com>
2021-10-03 20:50:27 +0300

    Work with Component properties

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        { name: 'Alex M.', salary: 800, increase: false },
+        { name: 'John S.', salary: 3000, increase: true },
+        { name: 'Edward R.', salary: 5000, increase: false },
+    ];
+
     return (
         <div className="app">
             <AppInfo />
@@ -16,7 +23,7 @@ const App = () => {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data} />
             <EmployeesAddForm />
         </div>
     );
