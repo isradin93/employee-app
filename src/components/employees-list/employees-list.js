8d07e2755663f94dfc8451dda55a17c6d1811ae9
Isradin <isradin.nurbek@gmail.com>
2021-11-25 19:45:44 +0300

    Change project name

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,9 +3,8 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({ data }) => {
-    console.log(data);
+
     const elements = data.map(item => {
-        console.log(item);
         return (
             <EmployeesListItem {...item} />
             // <EmployeesListItem name={item.name} salary={item.salary} />
