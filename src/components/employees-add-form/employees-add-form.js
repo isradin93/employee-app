1b4ae0f8cb6e0482ea1a810302a0a049fe6ccd53
Isradin <isradin.nurbek@gmail.com>
2021-12-13 20:06:18 +0300

    Add optimized method for 2 inputs in EmployeesAddForm component

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,23 +1,64 @@
+import { Component } from 'react/cjs/react.production.min';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Add new employee</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="What is his name?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="Salary in $?" />
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        };
+    }
 
-                <button type="submit"
-                    className="btn btn-outline-light">Add</button>
-            </form>
-        </div>
-    );
+    handleInputChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    };
+
+    // handleNameChange = e => {
+    //     this.setState({
+    //         name: e.target.value
+    //     });
+    // };
+
+    // handleSalaryChange = e => {
+    //     this.setState({
+    //         salary: e.target.value
+    //     });
+    // };
+
+    render() {
+
+        const { name, salary } = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Add new employee</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="What is his name?"
+                        name="name"
+                        value={name}
+                        onChange={this.handleInputChange}
+                    />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="Salary in $?"
+                        name="salary"
+                        value={salary}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <button type="submit"
+                        className="btn btn-outline-light">Add</button>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default EmployeesAddForm;
